999a0b540d3a3d6de9b9203dd4b602cf9ee8d569
Phil L <narvius@gmail.com>
2019-12-08 11:50:25 +0100

    implement day 8

---
+++ AoC2019/Day08.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AoC2019
+{
+    public class Day08 : ISolution
+    {
+        // Find the layer with the fewest zeroes, and calculate a checksum.
+        public string PartOne(string[] lines)
+        {
+            const int LayerSize = 25 * 6;
+            var groups = lines[0]
+                .Select((c, i) => (c, group: i / LayerSize))
+                .GroupBy(v => v.group)
+                .ToDictionary(k => k.Key, k => k.Select(pair => pair.c));
+
+            var leastZeroes = groups.Values.OrderBy(v => v.Count(c => c == '0')).First();
+            return (leastZeroes.Count(c => c == '1') * leastZeroes.Count(c => c == '2')).ToString();
+        }
+
+        // Merge all the layers in the image data, and interpret the final image.
+        // I'll skip the part where I automatically recognize the image contents, lol.
+        public string PartTwo(string[] lines)
+        {
+            const int LayerSize = 25 * 6;
+            var line = lines[0];
+            var result = new char[LayerSize];
+
+            for (int i = 0; i < LayerSize; i++)
+                for (int j = i; j < line.Length; j += LayerSize)
+                    if (line[j] != '2')
+                    {
+                        result[i] = line[j] == '1' ? '*' : ' ';
+                        break;
+                    }
+
+            File.WriteAllLines("Day08_output.txt", Enumerable.Range(0, 6).Select(i => new string(result, 25 * i, 25)));
+            return "check file 'Day08_output.txt'";
+        }
+    }
+}
