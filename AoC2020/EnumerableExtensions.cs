bf489907521b1cc1e2f16a060979419b92f18ac4
Filip Lange <narvius@gmail.com>
2020-12-04 10:36:50 +0100

    improve implementation of ChunkBy

--- AoC2020/EnumerableExtensions.cs
+++ AoC2020/EnumerableExtensions.cs
@@ -32,26 +32,33 @@ namespace AoC2020
         /// <summary>
         /// Subdivides an enumeration into chunks, with the final element of each chunk being an item that matches the given predicate.
         /// </summary>
-        /// <remarks>The final chunk will not be terminated by an item matching the predicate. It may be an empty chunk.</remarks>
+        /// <remarks>The final chunk might not be terminated by an item matching the predicate.</remarks>
         /// <param name="predicate">Used to decide whether an item is the end of a chunk.</param>
         /// <returns>The original enumeration, but subdivided into chunks.</returns>
         public static IEnumerable<IEnumerable<T>> ChunkBy<T>(this IEnumerable<T> @this, Predicate<T> predicate)
         {
             var enumerator = @this.GetEnumerator();
             bool hadItem = true;
+            bool emptyChunk = false;
 
             IEnumerable<T> BuildChunk()
             {
+                emptyChunk = true;
                 while (hadItem = enumerator.MoveNext())
                 {
                     yield return enumerator.Current;
+                    emptyChunk = false;
                     if (predicate(enumerator.Current))
                         yield break;
                 }
             }
 
             while (hadItem)
-                yield return BuildChunk();
+            {
+                var chunk = BuildChunk();
+                if (!emptyChunk)
+                    yield return chunk;
+            }
         }
     }
 }
