68bd604ce3bb9ca0fe87ccb20b7bf7d1f9d70025
Phil <narvius@gmail.com>
2021-11-19 13:10:07 +0100

    improve day 2015-15 solution

--- rust/solutions/src/aoc2015/day15.rs
+++ rust/solutions/src/aoc2015/day15.rs
@@ -34,36 +34,42 @@ pub fn part2(input: &[&str]) -> anyhow::Result<String> {
     // Brute force lmao. The calorie restriction actually vastly limits the search space, thus
     // making brute force more viable than in part 1!
 
+    // Recursively loops through all possible `choices` that match 
+    fn find_best_recipe_score(index: usize, ingredients: &[Vec<i32>], choices: &mut [i32], best: &mut i32, slots: i32, calories: i32) {
+        if index == ingredients.len() - 1 {
+            // Last ingredient doesn't need to be looped: Instead we just give all remaining slots
+            // to it, since we HAVE to use exactly 100. Then, only use the recipe if that actually
+            // matches the calorie count we want.
+            choices[index] = slots;
+            let calories: i32 = (0..ingredients.len()).map(|i| ingredients[i][4] * choices[i]).sum();
+            if calories == 500 {
+                *best = std::cmp::max(*best, score(&ingredients, &choices));
+            }
+        } else {
+            // Loop through this level of possible choices, then recurse with the next index to
+            // loop through that. Ultimately, this will result in all ingredients (except the last)
+            // getting their own loop.
+            for choice in 0..=std::cmp::min(slots, calories / ingredients[index][4]) {
+                choices[index] = choice;
+                find_best_recipe_score(index + 1, ingredients, choices, best, slots - choice, calories - choice * ingredients[index][4]);
+            }
+        }
+    }
+
     let ingredients =  {
-        // sort by calories descending, to minimize the number of loops we're doing later
+        // sort by calories descending, to minimize the number of iterations in outer loops
+        // we're doing later
         let mut temp = input.iter().map(|s| parse_line(s).unwrap()).collect::<Vec<_>>();
         temp.as_mut_slice().sort_by_key(|i| 100 - i[4]);
         temp
     };
 
-    let mut best = vec![1; 4];
-    let mut best_score = 0;
-    for i in 0..maximum_possible_units(&ingredients, &[]) {
-        for j in 0..maximum_possible_units(&ingredients, &[i]) {
-            for k in 0..maximum_possible_units(&ingredients, &[i, j]) {
-                let m = 100 - (i + j + k);
-                let counts = &[i, j, k, m];
-                let calories: i32 = (0..ingredients.len()).map(|i| ingredients[i][4] * counts[i]).sum();
-                if calories != 500 {
-                    continue;
-                } else {
-                    let score = score(&ingredients.as_slice(), counts);
-                    if score > best_score {
-                        best.clear();
-                        best.extend_from_slice(counts);
-                        best_score = score;
-                    }
-                }
-            }
-        }
-    }
+    let mut best = 0;
+    let mut choices = vec![0; ingredients.len()];
 
-    Ok(best_score.to_string())
+    find_best_recipe_score(0, &ingredients.as_slice(), &mut choices.as_mut_slice(), &mut best, 100, 500);
+
+    Ok(best.to_string())
 }
 
 /// Given a list of amount choices made so far, returns an upper bounds for how many you could
@@ -99,4 +105,38 @@ fn parse_line(s: &str) -> Option<Vec<i32>> {
     let calories = matches.next().and_then(|m| m.as_str().parse().ok())?;
 
     Some(vec![capacity, durability, flavor, texture, calories])
+}
+
+// Old solution. It assumed there will always be exactly 4 ingredients.
+fn _part2_first_solution(input: &[&str]) -> anyhow::Result<String> {
+    let ingredients =  {
+        // sort by calories descending, to minimize the number of loops we're doing later
+        let mut temp = input.iter().map(|s| parse_line(s).unwrap()).collect::<Vec<_>>();
+        temp.as_mut_slice().sort_by_key(|i| 100 - i[4]);
+        temp
+    };
+
+    let mut best = vec![1; 4];
+    let mut best_score = 0;
+    for i in 0..maximum_possible_units(&ingredients, &[]) {
+        for j in 0..maximum_possible_units(&ingredients, &[i]) {
+            for k in 0..maximum_possible_units(&ingredients, &[i, j]) {
+                let m = 100 - (i + j + k);
+                let counts = &[i, j, k, m];
+                let calories: i32 = (0..ingredients.len()).map(|i| ingredients[i][4] * counts[i]).sum();
+                if calories != 500 {
+                    continue;
+                } else {
+                    let score = score(&ingredients.as_slice(), counts);
+                    if score > best_score {
+                        best.clear();
+                        best.extend_from_slice(counts);
+                        best_score = score;
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(best_score.to_string())
 }
