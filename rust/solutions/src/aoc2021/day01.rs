995aee59bd78e9c1e9d1adc7d2824e5bf54a6bf9
Phil <narvius@gmail.com>
2021-12-01 15:53:11 +0100

    simplify day 2021-01

--- rust/solutions/src/aoc2021/day01.rs
+++ rust/solutions/src/aoc2021/day01.rs
@@ -1,9 +1,8 @@
 /// In the input file, count the number of lines that have a larger value than the preceding line.
 pub fn part1(input: &[&str]) -> anyhow::Result<String> {
     let depths = input.iter().map(|&s| s.parse::<u32>().unwrap()).collect::<Vec<_>>();
-    Ok(depths.iter()
-        .zip(depths.iter().skip(1))
-        .filter(|&(a, b)| b > a)
+    Ok(depths.windows(2)
+        .filter(|w| w[1] > w[0])
         .count().to_string())
 }
 
@@ -12,8 +11,7 @@ pub fn part1(input: &[&str]) -> anyhow::Result<String> {
 pub fn part2(input: &[&str]) -> anyhow::Result<String> {
     let depths = input.iter().map(|&s| s.parse::<u32>().unwrap()).collect::<Vec<_>>();
     let window_sums = depths.windows(3).map(|w| w.iter().sum()).collect::<Vec<u32>>();
-    Ok(window_sums.iter()
-        .zip(window_sums.iter().skip(1))
-        .filter(|&(a, b)| b > a)
+    Ok(window_sums.windows(2)
+        .filter(|w| w[1] > w[0])
         .count().to_string())
 }
